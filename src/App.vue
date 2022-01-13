d0624df46b9196f792631069036b5c0ef97411f6
“[atefeh <[atefeh@buzzjective.com]>
2022-01-13 18:27:08 +0330

    add materialIcon, change on components

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,17 @@
   </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app{
+  margin-top: 60px;
+  color: #2c3e50;
+  text-align: center;
+}
+</style>
+
---
+++ src/components/MoviesCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container mx-auto text-center mt-6 md:container md:mx-auto">
+    <div class="grid grid-cols-4 gap-4">
+      <div class="border-solid border-2 border-gray-400 rounded-md flex">
+        <div class="bg-gray-200 ">
+          <img src="@/assets/farmer.png" class="">
+          <div class="details">
+            <h3>Inception</h3>
+            <i class="fas fa-calendar"></i>
+            <span>2-1-2021</span>
+            <ul>
+              <li>Drama</li>
+              <li>Action</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- ... -->
+      <div>9</div>
+      <!-- ... -->
+      <div>9</div>
+      <!-- ... -->
+      <div>9</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "MoviesCard",
+
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
+  mounted() {
+    console.log(this.fethchData());
+  },
+
+  methods: {
+    fethchData() {
+      axios.get("https://api.themoviedb.org/3/discover/movie").then((result) => {
+        this.movies = result.data;
+      });
+    },
+  },
+};
+</script>
